0cdcf55415b415538cd264f0add6bf7a12a7769a
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-12-01 21:36:42 -0500

    XMDEV-521: Remove legacy rails app (#11)

--- backend/app/models/device.rb
+++
@@ -1,7 +0,0 @@
-class Device < ApplicationRecord
-    validates :vendor, presence: true
-    validates :model_num, presence: true, uniqueness: true
-    validates :market_name, presence: true
-
-    has_many :softwares, dependent: :destroy
-end
