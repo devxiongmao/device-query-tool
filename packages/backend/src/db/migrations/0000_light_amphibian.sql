b3a9a1cf080d4783fc3a6394af8b4fa1a5f234c2
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-12-06 23:53:40 -0500

    XMDEV-532: Adds seed data (#21)

--- packages/backend/src/db/migrations/0000_light_amphibian.sql
+++ packages/backend/src/db/migrations/0000_light_amphibian.sql
@@ -1,21 +1,20 @@
 CREATE TABLE "BAND" (
-	"band_id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "BAND_band_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "BAND_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
 	"band_number" varchar(20) NOT NULL,
-	"technology" varchar(20) NOT NULL,
-	"frequency_range" varchar(100) NOT NULL,
-	"band_class" varchar(50)
+	"technology" varchar(20) NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "COMBO" (
-	"combo_id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "COMBO_combo_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "COMBO_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
 	"name" varchar(200) NOT NULL,
 	"technology" varchar(20) NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "COMBO_BAND" (
-	"combo_id" integer NOT NULL,
-	"band_id" integer NOT NULL,
-	"position" integer NOT NULL,
+	"combo_id" bigint NOT NULL,
+	"band_id" bigint NOT NULL,
+	"dl_band_class" varchar(50),
+	"ul_band_class" varchar(50),
 	CONSTRAINT "COMBO_BAND_combo_id_band_id_pk" PRIMARY KEY("combo_id","band_id")
 );
 --> statement-breakpoint
@@ -32,21 +31,21 @@ CREATE TABLE "DEVICE" (
 CREATE TABLE "DEVICE_SOFTWARE_BAND" (
 	"device_id" bigint NOT NULL,
 	"software_id" bigint NOT NULL,
-	"band_id" integer NOT NULL,
+	"band_id" bigint NOT NULL,
 	CONSTRAINT "DEVICE_SOFTWARE_BAND_device_id_software_id_band_id_pk" PRIMARY KEY("device_id","software_id","band_id")
 );
 --> statement-breakpoint
 CREATE TABLE "DEVICE_SOFTWARE_COMBO" (
 	"device_id" bigint NOT NULL,
 	"software_id" bigint NOT NULL,
-	"combo_id" integer NOT NULL,
+	"combo_id" bigint NOT NULL,
 	CONSTRAINT "DEVICE_SOFTWARE_COMBO_device_id_software_id_combo_id_pk" PRIMARY KEY("device_id","software_id","combo_id")
 );
 --> statement-breakpoint
 CREATE TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" (
 	"device_id" bigint NOT NULL,
 	"software_id" bigint NOT NULL,
-	"provider_id" integer NOT NULL,
+	"provider_id" bigint NOT NULL,
 	"feature_id" bigint NOT NULL,
 	CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_device_id_software_id_provider_id_feature_id_pk" PRIMARY KEY("device_id","software_id","provider_id","feature_id")
 );
@@ -60,25 +59,25 @@ CREATE TABLE "FEATURE" (
 );
 --> statement-breakpoint
 CREATE TABLE "PROVIDER" (
-	"provider_id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "PROVIDER_provider_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "PROVIDER_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
 	"name" varchar(100) NOT NULL,
 	"country" varchar(100) NOT NULL,
 	"network_type" varchar(50) NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" (
-	"provider_id" integer NOT NULL,
+	"provider_id" bigint NOT NULL,
 	"device_id" bigint NOT NULL,
 	"software_id" bigint NOT NULL,
-	"band_id" integer NOT NULL,
+	"band_id" bigint NOT NULL,
 	CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_provider_id_device_id_software_id_band_id_pk" PRIMARY KEY("provider_id","device_id","software_id","band_id")
 );
 --> statement-breakpoint
 CREATE TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" (
-	"provider_id" integer NOT NULL,
+	"provider_id" bigint NOT NULL,
 	"device_id" bigint NOT NULL,
 	"software_id" bigint NOT NULL,
-	"combo_id" integer NOT NULL,
+	"combo_id" bigint NOT NULL,
 	CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_provider_id_device_id_software_id_combo_id_pk" PRIMARY KEY("provider_id","device_id","software_id","combo_id")
 );
 --> statement-breakpoint
@@ -95,26 +94,26 @@ CREATE TABLE "SOFTWARE" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "COMBO_BAND" ADD CONSTRAINT "COMBO_BAND_combo_id_COMBO_combo_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("combo_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "COMBO_BAND" ADD CONSTRAINT "COMBO_BAND_band_id_BAND_band_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("band_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "COMBO_BAND" ADD CONSTRAINT "COMBO_BAND_combo_id_COMBO_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "COMBO_BAND" ADD CONSTRAINT "COMBO_BAND_band_id_BAND_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "DEVICE_SOFTWARE_BAND_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "DEVICE_SOFTWARE_BAND_software_id_SOFTWARE_id_fk" FOREIGN KEY ("software_id") REFERENCES "public"."SOFTWARE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "DEVICE_SOFTWARE_BAND_band_id_BAND_band_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("band_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "DEVICE_SOFTWARE_BAND_band_id_BAND_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "DEVICE_SOFTWARE_COMBO_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "DEVICE_SOFTWARE_COMBO_software_id_SOFTWARE_id_fk" FOREIGN KEY ("software_id") REFERENCES "public"."SOFTWARE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "DEVICE_SOFTWARE_COMBO_combo_id_COMBO_combo_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("combo_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "DEVICE_SOFTWARE_COMBO_combo_id_COMBO_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" ADD CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" ADD CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_software_id_SOFTWARE_id_fk" FOREIGN KEY ("software_id") REFERENCES "public"."SOFTWARE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" ADD CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_provider_id_PROVIDER_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("provider_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" ADD CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_provider_id_PROVIDER_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "DEVICE_SOFTWARE_PROVIDER_FEATURE" ADD CONSTRAINT "DEVICE_SOFTWARE_PROVIDER_FEATURE_feature_id_FEATURE_id_fk" FOREIGN KEY ("feature_id") REFERENCES "public"."FEATURE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_provider_id_PROVIDER_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("provider_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_provider_id_PROVIDER_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_software_id_SOFTWARE_id_fk" FOREIGN KEY ("software_id") REFERENCES "public"."SOFTWARE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_band_id_BAND_band_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("band_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_provider_id_PROVIDER_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("provider_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_BAND" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_BAND_band_id_BAND_id_fk" FOREIGN KEY ("band_id") REFERENCES "public"."BAND"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_provider_id_PROVIDER_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."PROVIDER"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_software_id_SOFTWARE_id_fk" FOREIGN KEY ("software_id") REFERENCES "public"."SOFTWARE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_combo_id_COMBO_combo_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("combo_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "PROVIDER_DEVICE_SOFTWARE_COMBO" ADD CONSTRAINT "PROVIDER_DEVICE_SOFTWARE_COMBO_combo_id_COMBO_id_fk" FOREIGN KEY ("combo_id") REFERENCES "public"."COMBO"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "SOFTWARE" ADD CONSTRAINT "SOFTWARE_device_id_DEVICE_id_fk" FOREIGN KEY ("device_id") REFERENCES "public"."DEVICE"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "idx_dsb_device_software" ON "DEVICE_SOFTWARE_BAND" USING btree ("device_id","software_id");--> statement-breakpoint
 CREATE INDEX "idx_dsb_band_lookup" ON "DEVICE_SOFTWARE_BAND" USING btree ("band_id");--> statement-breakpoint
