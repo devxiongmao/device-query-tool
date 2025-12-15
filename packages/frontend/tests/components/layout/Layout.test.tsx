ac62d1b025e741be51d7e3085870e4491cbda434
Jon Psaila <jonathan_psaila@outlook.com>
2025-12-15 18:56:02 -0500

    test

--- packages/frontend/tests/components/layout/Layout.test.tsx
+++ packages/frontend/tests/components/layout/Layout.test.tsx
@@ -1,8 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { axe, toHaveNoViolations } from "jest-axe";
-
 import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { axe, toHaveNoViolations } from "jest-axe";
 import { Layout } from "../../../src/components/layout/Layout";
 
 expect.extend(toHaveNoViolations);
